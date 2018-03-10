ae0161ff6c10eacd044ff7c4078f728d9803a8e5
Jérémy TELLIEZ <telliez@unistra.fr>
2018-03-10 17:56:06 -0500

    Ajout boucle de jeu basique et compteur de fps pour demo

---
+++ app/src/main/java/uqac/dim/houla/GameObject.java
@@ -0,0 +1,12 @@
+package uqac.dim.houla;
+
+import android.graphics.Canvas;
+
+/**
+ * Created by User on 10/03/2018.
+ */
+
+public interface GameObject {
+    public void draw(Canvas canvas);
+    public void update();
+}
--- app/src/main/java/uqac/dim/houla/GameView.java
+++ app/src/main/java/uqac/dim/houla/GameView.java
@@ -2,6 +2,9 @@ package uqac.dim.houla;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -13,12 +16,19 @@ import android.view.SurfaceView;
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
     private MainThread thread;
 
+    private Player player;
+    private Point playerPoint;
+
     public GameView(Context context){
         super(context);
 
         getHolder().addCallback(this);
 
         thread = new MainThread(getHolder(), this);
+
+        player = new Player(new Rect(100,100,200,200), Color.rgb(255,0,0));
+
+        playerPoint = new Point(150,150);
 
         setFocusable(true);
     }
@@ -50,15 +60,28 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                playerPoint.set((int)event.getX(),(int)event.getY());
+
+        }
+
+
+        return true;
+        //return super.onTouchEvent(event);
     }
 
     public void update(){
-
+        player.update(playerPoint);
     }
 
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
+
+        canvas.drawColor(Color.WHITE);
+
+        player.draw(canvas);
     }
 }
