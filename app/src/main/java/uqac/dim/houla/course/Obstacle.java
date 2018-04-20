24d3a26bd9ee62c99ff8c55423281785984378a5
Jérémy TELLIEZ <telliez@unistra.fr>
2018-04-20 19:27:00 -0400

    Renommage des images du joueur du minijeu course pour suivre des conventions de nommage [NomMinijeu]_[Objet]_[NomRessource]

--- app/src/main/java/uqac/dim/houla/course/Obstacle.java
+++ app/src/main/java/uqac/dim/houla/course/Obstacle.java
@@ -1,9 +1,14 @@
 package uqac.dim.houla.course;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
+
+import uqac.dim.houla.Constant;
+import uqac.dim.houla.R;
 
 /**
  * Created by User on 10/03/2018.
@@ -14,6 +19,9 @@ public class Obstacle implements GameObject {
     private Rect rectangle;
     private int color;
 
+    private Animation idle;
+    private AnimationManager am;
+
     public Rect getRectangle(){
         return rectangle;
     }
@@ -21,6 +29,12 @@ public class Obstacle implements GameObject {
     public Obstacle(int height, int color, int startX, int startY){
         this.rectangle = new Rect(startX-100, startY, startX, startY+height );
         this.color = color;
+
+        BitmapFactory bf = new BitmapFactory();
+        Bitmap[] idleBit = new Bitmap[3];
+        idleBit[0] = bf.decodeResource(Constant.CURRENT_CONTEXT.getResources(), R.drawable.course_player_idle0);
+        idleBit[1] = bf.decodeResource(Constant.CURRENT_CONTEXT.getResources(), R.drawable.course_player_idle1);
+        idleBit[2] = bf.decodeResource(Constant.CURRENT_CONTEXT.getResources(), R.drawable.course_player_idle2);
     }
 
     public boolean playerCollide(Player player){
